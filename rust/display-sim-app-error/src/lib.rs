e1cbf99391a1d5c914fc1f1535d1040c3402ca1e
José manuel Barroso Galindo <theypsilon@gmail.com>
2019-11-18 01:27:46 +0100

    Moving error code to lib.rs because it's fairly little.

--- rust/display-sim-app-error/src/lib.rs
+++ rust/display-sim-app-error/src/lib.rs
@@ -13,5 +13,54 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>. */
 
-mod app_error;
-pub use app_error::*;
+use std::{
+    error::Error,
+    fmt::{Display, Formatter, Result},
+};
+
+pub type AppResult<T> = std::result::Result<T, AppError>;
+
+#[derive(Clone, Debug)]
+pub struct AppError {
+    err: String,
+}
+
+impl AppError {
+    pub fn new(err: String) -> Self {
+        AppError { err }
+    }
+}
+
+impl Error for AppError {}
+
+impl Display for AppError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        write!(f, "{}", self.err)
+    }
+}
+
+#[cfg(target_arch = "wasm32")]
+impl From<AppError> for wasm_bindgen::JsValue {
+    fn from(e: AppError) -> Self {
+        e.err.into()
+    }
+}
+
+#[cfg(target_arch = "wasm32")]
+impl From<wasm_bindgen::JsValue> for AppError {
+    fn from(o: wasm_bindgen::JsValue) -> Self {
+        AppError { err: format!("{:#?}", o) }
+    }
+}
+
+impl From<std::string::String> for AppError {
+    fn from(string: std::string::String) -> Self {
+        AppError { err: string }
+    }
+}
+
+impl<'a> From<&'a str> for AppError {
+    fn from(string: &'a str) -> Self {
+        AppError { err: string.into() }
+    }
+}
