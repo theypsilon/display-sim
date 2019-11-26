bd2522b118a9ec6ac24f5edf12f39b83c5da99d6
José manuel Barroso Galindo <theypsilon@gmail.com>
2019-11-26 03:59:15 +0100

    Improved finished, screenshot fixed.

--- rust/display-sim-render/src/simulation_draw.rs
+++ rust/display-sim-render/src/simulation_draw.rs
@@ -39,178 +39,179 @@ impl<'a> SimulationDrawer<'a> {
             return Ok(());
         }
 
-        let gl = &self.materials.gl;
+        let filters = &self.res.filters;
+        let output = &self.res.output;
+
+        let materials = &mut self.materials;
+        let gl = &materials.gl;
+
+        let resolution_width = filters.internal_resolution.width();
+        let resolution_height = filters.internal_resolution.height();
+
+        let viewport_width = self.res.video.viewport_size.width;
+        let viewport_height = self.res.video.viewport_size.height;
 
         if self.res.video.needs_buffer_data_load {
-            self.materials.pixels_render.load_image(&self.res.video);
+            materials.pixels_render.load_image(&self.res.video);
         }
 
-        self.materials.main_buffer_stack.set_depthbuffer(self.res.output.pixel_have_depth)?;
+        materials.main_buffer_stack.set_depthbuffer(output.pixel_have_depth)?;
+        materials.main_buffer_stack.set_resolution(resolution_width, resolution_height)?;
+        materials.main_buffer_stack.set_interpolation(match filters.texture_interpolation {
+            TextureInterpolation::Linear => glow::LINEAR,
+            TextureInterpolation::Nearest => glow::NEAREST,
+        })?;
 
-        self.materials
-            .main_buffer_stack
-            .set_resolution(self.res.filters.internal_resolution.width(), self.res.filters.internal_resolution.height())?;
-
-        self.materials
-            .main_buffer_stack
-            .set_interpolation(match self.res.filters.texture_interpolation {
-                TextureInterpolation::Linear => glow::LINEAR,
-                TextureInterpolation::Nearest => glow::NEAREST,
-            })?;
-
-        self.materials.main_buffer_stack.push()?;
-        self.materials.main_buffer_stack.push()?;
-        self.materials.main_buffer_stack.bind_current()?;
+        materials.main_buffer_stack.push()?;
+        materials.main_buffer_stack.push()?;
+        materials.main_buffer_stack.bind_current()?;
 
         gl.clear_color(0.0, 0.0, 0.0, 0.0);
         gl.clear(glow::COLOR_BUFFER_BIT | glow::DEPTH_BUFFER_BIT);
 
         let view = self.res.camera.get_view();
         let position = self.res.camera.get_position();
-        let projection = self
-            .res
-            .camera
-            .get_projection(self.res.video.viewport_size.width as f32, self.res.video.viewport_size.height as f32);
 
-        for hl_idx in 0..self.res.filters.horizontal_lpp {
-            for vl_idx in 0..self.res.filters.vertical_lpp {
-                for color_idx in 0..self.res.output.color_splits {
-                    if let ColorChannels::Overlapping = self.res.filters.color_channels {
-                        self.materials.main_buffer_stack.push()?;
-                        self.materials.main_buffer_stack.bind_current()?;
+        let projection = if self.res.screenshot_trigger.is_triggered {
+            self.res.camera.get_projection(resolution_width as f32, resolution_height as f32)
+        } else {
+            self.res.camera.get_projection(viewport_width as f32, viewport_height as f32)
+        };
+
+        for hl_idx in 0..filters.horizontal_lpp {
+            for vl_idx in 0..filters.vertical_lpp {
+                for color_idx in 0..output.color_splits {
+                    if let ColorChannels::Overlapping = filters.color_channels {
+                        materials.main_buffer_stack.push()?;
+                        materials.main_buffer_stack.bind_current()?;
                         if vl_idx == 0 && hl_idx == 0 {
                             gl.clear(glow::COLOR_BUFFER_BIT | glow::DEPTH_BUFFER_BIT);
                         }
                     }
-                    self.materials.pixels_render.render(PixelsUniform {
-                        shadow_kind: self.res.filters.pixel_shadow_shape_kind.value,
-                        geometry_kind: self.res.filters.pixels_geometry_kind,
+                    materials.pixels_render.render(PixelsUniform {
+                        shadow_kind: filters.pixel_shadow_shape_kind.value,
+                        geometry_kind: filters.pixels_geometry_kind,
                         view: &matrix_to_16_f32(view),
                         projection: &matrix_to_16_f32(projection),
-                        ambient_strength: self.res.output.ambient_strength,
-                        contrast_factor: self.res.filters.extra_contrast,
-                        light_color: &self.res.output.light_color[color_idx],
-                        extra_light: &self.res.output.extra_light,
+                        ambient_strength: output.ambient_strength,
+                        contrast_factor: filters.extra_contrast,
+                        light_color: &output.light_color[color_idx],
+                        extra_light: &output.extra_light,
                         light_pos: &vec_to_3_f32(position),
-                        screen_curvature: self.res.output.screen_curvature_factor,
-                        pixel_spread: &self.res.output.pixel_spread,
-                        pixel_scale: &self
-                            .res
-                            .output
+                        screen_curvature: output.screen_curvature_factor,
+                        pixel_spread: &output.pixel_spread,
+                        pixel_scale: &output
                             .pixel_scale_foreground
-                            .get(vl_idx * self.res.filters.horizontal_lpp + hl_idx)
+                            .get(vl_idx * filters.horizontal_lpp + hl_idx)
                             .expect("Bad pixel_scale_foreground")[color_idx],
-                        pixel_pulse: self.res.output.pixels_pulse,
-                        pixel_offset: &self
-                            .res
-                            .output
+                        pixel_pulse: output.pixels_pulse,
+                        pixel_offset: &output
                             .pixel_offset_foreground
-                            .get(vl_idx * self.res.filters.horizontal_lpp + hl_idx)
+                            .get(vl_idx * filters.horizontal_lpp + hl_idx)
                             .expect("Bad pixel_offset_foreground")[color_idx],
-                        height_modifier_factor: self.res.output.height_modifier_factor,
+                        height_modifier_factor: output.height_modifier_factor,
                     });
                 }
-                if let ColorChannels::Overlapping = self.res.filters.color_channels {
-                    self.materials.main_buffer_stack.pop()?;
-                    self.materials.main_buffer_stack.pop()?;
-                    self.materials.main_buffer_stack.pop()?;
+                if let ColorChannels::Overlapping = filters.color_channels {
+                    materials.main_buffer_stack.pop()?;
+                    materials.main_buffer_stack.pop()?;
+                    materials.main_buffer_stack.pop()?;
                 }
             }
         }
 
-        if let ColorChannels::Overlapping = self.res.filters.color_channels {
-            self.materials.main_buffer_stack.bind_current()?;
+        if let ColorChannels::Overlapping = filters.color_channels {
+            materials.main_buffer_stack.bind_current()?;
             gl.active_texture(glow::TEXTURE0 + 0);
-            gl.bind_texture(glow::TEXTURE_2D, self.materials.main_buffer_stack.get_nth(1)?.texture());
+            gl.bind_texture(glow::TEXTURE_2D, materials.main_buffer_stack.get_nth(1)?.texture());
             gl.active_texture(glow::TEXTURE0 + 1);
-            gl.bind_texture(glow::TEXTURE_2D, self.materials.main_buffer_stack.get_nth(2)?.texture());
+            gl.bind_texture(glow::TEXTURE_2D, materials.main_buffer_stack.get_nth(2)?.texture());
             gl.active_texture(glow::TEXTURE0 + 2);
-            gl.bind_texture(glow::TEXTURE_2D, self.materials.main_buffer_stack.get_nth(3)?.texture());
+            gl.bind_texture(glow::TEXTURE_2D, materials.main_buffer_stack.get_nth(3)?.texture());
 
-            self.materials.rgb_render.render();
+            materials.rgb_render.render();
 
             gl.active_texture(glow::TEXTURE0 + 0);
         }
 
-        self.materials.main_buffer_stack.push()?;
-        self.materials.main_buffer_stack.bind_current()?;
+        materials.main_buffer_stack.push()?;
+        materials.main_buffer_stack.bind_current()?;
         gl.clear(glow::COLOR_BUFFER_BIT | glow::DEPTH_BUFFER_BIT);
 
-        if self.res.output.showing_background {
-            self.materials.bg_buffer_stack.set_resolution(1920 / 2, 1080 / 2)?;
-            self.materials.bg_buffer_stack.set_depthbuffer(false)?;
-            self.materials.bg_buffer_stack.set_interpolation(glow::LINEAR)?;
-            self.materials.bg_buffer_stack.push()?;
-            self.materials.bg_buffer_stack.bind_current()?;
+        if output.showing_background {
+            materials.bg_buffer_stack.set_resolution(1920 / 2, 1080 / 2)?;
+            materials.bg_buffer_stack.set_depthbuffer(false)?;
+            materials.bg_buffer_stack.set_interpolation(glow::LINEAR)?;
+            materials.bg_buffer_stack.push()?;
+            materials.bg_buffer_stack.bind_current()?;
             gl.clear(glow::COLOR_BUFFER_BIT | glow::DEPTH_BUFFER_BIT);
-            for hl_idx in 0..self.res.filters.horizontal_lpp {
-                for vl_idx in 0..self.res.filters.vertical_lpp {
-                    self.materials.pixels_render.render(PixelsUniform {
+            for hl_idx in 0..filters.horizontal_lpp {
+                for vl_idx in 0..filters.vertical_lpp {
+                    materials.pixels_render.render(PixelsUniform {
                         shadow_kind: 0,
-                        geometry_kind: self.res.filters.pixels_geometry_kind,
+                        geometry_kind: filters.pixels_geometry_kind,
                         view: &matrix_to_16_f32(view),
                         projection: &matrix_to_16_f32(projection),
-                        ambient_strength: self.res.output.ambient_strength,
-                        contrast_factor: self.res.filters.extra_contrast,
-                        light_color: &self.res.output.light_color_background,
+                        ambient_strength: output.ambient_strength,
+                        contrast_factor: filters.extra_contrast,
+                        light_color: &output.light_color_background,
                         extra_light: &[0.0, 0.0, 0.0],
                         light_pos: &vec_to_3_f32(position),
-                        pixel_spread: &self.res.output.pixel_spread,
-                        pixel_scale: &self.res.output.pixel_scale_background[vl_idx * self.res.filters.horizontal_lpp + hl_idx],
-                        screen_curvature: self.res.output.screen_curvature_factor,
-                        pixel_pulse: self.res.output.pixels_pulse,
-                        pixel_offset: &self.res.output.pixel_offset_background[vl_idx * self.res.filters.horizontal_lpp + hl_idx],
+                        pixel_spread: &output.pixel_spread,
+                        pixel_scale: &output.pixel_scale_background[vl_idx * filters.horizontal_lpp + hl_idx],
+                        screen_curvature: output.screen_curvature_factor,
+                        pixel_pulse: output.pixels_pulse,
+                        pixel_offset: &output.pixel_offset_background[vl_idx * filters.horizontal_lpp + hl_idx],
                         height_modifier_factor: 0.0,
                     });
                 }
             }
-            let source = (*self.materials.bg_buffer_stack.get_current()?).clone();
-            let target = self.materials.main_buffer_stack.get_current()?;
-            self.materials.blur_render.render(&mut self.materials.bg_buffer_stack, &source, &target, 6)?;
-            self.materials.bg_buffer_stack.pop()?;
+            let source = (*materials.bg_buffer_stack.get_current()?).clone();
+            let target = materials.main_buffer_stack.get_current()?;
+            materials.blur_render.render(&mut materials.bg_buffer_stack, &source, &target, 6)?;
+            materials.bg_buffer_stack.pop()?;
         }
-        self.materials.main_buffer_stack.pop()?;
-        self.materials.main_buffer_stack.pop()?;
-        self.materials.main_buffer_stack.bind_current()?;
+        materials.main_buffer_stack.pop()?;
+        materials.main_buffer_stack.pop()?;
+        materials.main_buffer_stack.bind_current()?;
         gl.clear(glow::COLOR_BUFFER_BIT | glow::DEPTH_BUFFER_BIT);
 
         gl.active_texture(glow::TEXTURE0 + 0);
-        gl.bind_texture(glow::TEXTURE_2D, self.materials.main_buffer_stack.get_nth(1)?.texture());
+        gl.bind_texture(glow::TEXTURE_2D, materials.main_buffer_stack.get_nth(1)?.texture());
         gl.active_texture(glow::TEXTURE0 + 1);
-        gl.bind_texture(glow::TEXTURE_2D, self.materials.main_buffer_stack.get_nth(2)?.texture());
-        self.materials.background_render.render();
+        gl.bind_texture(glow::TEXTURE_2D, materials.main_buffer_stack.get_nth(2)?.texture());
+        materials.background_render.render();
         gl.active_texture(glow::TEXTURE0 + 0);
 
-        if self.res.filters.blur_passes > 0 {
-            let target = self.materials.main_buffer_stack.get_current()?.clone();
-            self.materials
+        if filters.blur_passes > 0 {
+            let target = materials.main_buffer_stack.get_current()?.clone();
+            materials
                 .blur_render
-                .render(&mut self.materials.main_buffer_stack, &target, &target, self.res.filters.blur_passes)?;
+                .render(&mut materials.main_buffer_stack, &target, &target, filters.blur_passes)?;
         }
 
-        self.materials.screenshot_pixels = None;
+        materials.screenshot_pixels = None;
 
         if self.res.screenshot_trigger.is_triggered {
-            let width = self.res.filters.internal_resolution.width();
-            let height = self.res.filters.internal_resolution.height();
-            let pixels: Box<[u8]> = vec![0; (width * height * 4) as usize].into_boxed_slice();
-            self.materials.screenshot_pixels = Some(pixels);
-            self.ctx
-                .dispatcher()
-                .fire_screenshot(width, height, self.materials.screenshot_pixels.as_mut().expect("Screenshot bug"));
+            let pixels: Box<[u8]> = vec![0; (resolution_width * resolution_height * 4) as usize].into_boxed_slice();
+            materials.screenshot_pixels = Some(pixels);
+            match materials.screenshot_pixels {
+                Some(ref mut pixels) => self.ctx.dispatcher().fire_screenshot(resolution_width, resolution_height, pixels),
+                None => self.ctx.dispatcher().dispatch_log("Screenshot failed.".into()),
+            }
+            materials.main_buffer_stack.pop()?;
+            materials.main_buffer_stack.assert_no_stack()?;
+        } else {
+            materials.main_buffer_stack.pop()?;
+            materials.main_buffer_stack.assert_no_stack()?;
+
+            gl.bind_framebuffer(glow::FRAMEBUFFER, None);
+            gl.clear(glow::COLOR_BUFFER_BIT | glow::DEPTH_BUFFER_BIT);
+
+            gl.viewport(0, 0, viewport_width as i32, viewport_height as i32);
+
+            materials.internal_resolution_render.render(materials.main_buffer_stack.get_nth(1)?.texture());
         }
-
-        self.materials.main_buffer_stack.pop()?;
-        self.materials.main_buffer_stack.assert_no_stack()?;
-
-        gl.bind_framebuffer(glow::FRAMEBUFFER, None);
-        gl.clear(glow::COLOR_BUFFER_BIT | glow::DEPTH_BUFFER_BIT);
-
-        gl.viewport(0, 0, self.res.video.viewport_size.width as i32, self.res.video.viewport_size.height as i32);
-
-        self.materials
-            .internal_resolution_render
-            .render(self.materials.main_buffer_stack.get_nth(1)?.texture());
 
         check_error(&gl, line!())?;
 
